8709aa3f3631b46cf94122cc21c78e7617d4f662
Yakser <iaksanov2044@mail.ru>
2022-10-17 01:40:01 +0300

    Fixed user serialization and views

--- users/serializers.py
+++ users/serializers.py
@@ -5,15 +5,7 @@ from .models import CustomUser
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['email', 'username', 'password', 'password2']
-
-    def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError(
-                {'password': 'Password fields did not match'}
-            )
-
-        return attrs
+        fields = ('email', 'username', 'password')
 
     def create(self, validated_data):
         user = CustomUser(**validated_data)
