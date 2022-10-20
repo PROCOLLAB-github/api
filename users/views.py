c08a1443e32e71973dd2ffd914a9f62a97179915
Yakser <iaksanov2044@mail.ru>
2022-10-20 23:47:01 +0300

    Fixed pep8 warnings

--- users/views.py
+++ users/views.py
@@ -4,11 +4,14 @@ from django.contrib.auth import get_user_model
 from django.contrib.sites.shortcuts import get_current_site
 from django.urls import reverse
 from rest_framework.response import Response
-from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import permissions, status
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView,\
-    GenericAPIView
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView,
+    GenericAPIView,
+)
 
 from core.utils import Email
 from .serializers import UserSerializer
@@ -27,24 +30,22 @@ class UserList(ListCreateAPIView):
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
 
-        user = User.objects.get(email=serializer.data['email'])
+        user = User.objects.get(email=serializer.data["email"])
 
         token = RefreshToken.for_user(user).access_token
 
-        relative_link = reverse('account_email_verification_sent')
+        relative_link = reverse("account_email_verification_sent")
         current_site = get_current_site(request).domain
-        absolute_url = 'http://' + current_site + relative_link + "?token=" + str(token)
+        absolute_url = "http://" + current_site + relative_link + "?token=" + str(token)
 
-        email_body = 'Hi, {} {}! Use link below verify your email {}'.format(
-            user.first_name,
-            user.last_name,
-            absolute_url
+        email_body = "Hi, {} {}! Use link below verify your email {}".format(
+            user.first_name, user.last_name, absolute_url
         )
 
         data = {
-            'email_body': email_body,
-            'email_subject': 'Verify your email',
-            'to_email': user.email
+            "email_body": email_body,
+            "email_subject": "Verify your email",
+            "to_email": user.email,
         }
 
         Email.send_email(data)
@@ -60,21 +61,21 @@ class UserDetail(RetrieveUpdateDestroyAPIView):
 
 class VerifyEmail(GenericAPIView):
     def get(self, request):
-        token = request.GET.get('token')
+        token = request.GET.get("token")
         try:
-            payload = jwt.decode(jwt=token, key=settings.SECRET_KEY, algorithms=['HS256'])
-            user = User.objects.get(id=payload['user_id'])
+            payload = jwt.decode(jwt=token, key=settings.SECRET_KEY, algorithms=["HS256"])
+            user = User.objects.get(id=payload["user_id"])
             if not user.is_active:
                 user.is_active = True
                 user.save()
 
-            return Response({'email': 'Successfully activated'},
-                            status=status.HTTP_200_OK)
+            return Response(
+                {"email": "Successfully activated"}, status=status.HTTP_200_OK
+            )
 
-        except jwt.ExpiredSignatureError as identifier:
-            return Response({'error': 'Activate Expired'},
-                            status=status.HTTP_400_BAD_REQUEST)
-        except jwt.DecodeError as decode_error:
-            return Response({'error': 'Decode error'},
-                            status=status.HTTP_400_BAD_REQUEST)
-
+        except jwt.ExpiredSignatureError:
+            return Response(
+                {"error": "Activate Expired"}, status=status.HTTP_400_BAD_REQUEST
+            )
+        except jwt.DecodeError:
+            return Response({"error": "Decode error"}, status=status.HTTP_400_BAD_REQUEST)
