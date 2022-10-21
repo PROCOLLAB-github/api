f8ededf821527053ad2d32aa564b62ea09106b36
stolzor <ruslangataullin16@gmail.com>
2022-10-21 16:37:45 +0300

    fix db

--- procollab/settings.py
+++ procollab/settings.py
@@ -100,11 +100,10 @@ DB_SERVICE = config("DB_SERVICE", default="postgres", cast=str)
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "procollab_backend",
-        "USER": "postgres",
-        "PASSWORD": "123980",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+        "OPTIONS": {
+            "service": DB_SERVICE,
+            "passfile": "",
+        },
     }
 }
 
