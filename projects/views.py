f78913b99f70ed24d835c2aa4b1bdc595232bcf0
Mikhail Khromov <khromov05@gmail.com>
2022-10-23 22:32:46 +0300

    добавил возможность удалять коллабораторов из проекта, надо сделать DELETE /projects/<id>/collaborators

--- projects/views.py
+++ projects/views.py
@@ -1,11 +1,11 @@
-from core.permissions import IsProjectLeaderOrReadOnly, IsStaffOrReadOnly
 from rest_framework import generics, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from core.permissions import IsProjectLeaderOrReadOnly, IsStaffOrReadOnly
 from projects.helpers import VERBOSE_STEPS
 from projects.models import Project, Achievement
-from projects.serializers import ProjectSerializer, AchievementSerializer
+from projects.serializers import ProjectSerializer, AchievementSerializer, ProjectCollaboratorsSerializer
 
 
 class ProjectList(generics.ListCreateAPIView):
@@ -18,6 +18,30 @@ class ProjectDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class ProjectCollaborators(generics.GenericAPIView):
+    """
+        Project collaborator delete view
+    """
+    # maybe should get/add collaborators here also? (e.g. retrieve/create, get/post methods)
+
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Project.objects.all()
+    serializer_class = ProjectCollaboratorsSerializer
+
+    def delete(self, request, pk: int):
+        return self.remove(request)
+
+    def remove(self, request):
+        m2m_manager = self.get_object().collaborators
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        collaborators = serializer.validated_data['collaborators']
+        for user in collaborators:
+            # note: doesn't raise an error when we try to delete someone who isn't a collaborator
+            m2m_manager.remove(user)
+        return Response(status=200)
 
 
 class ProjectSteps(APIView):
