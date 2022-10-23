c5a67b971ab73d53ebaf0454149c8e11148cff40
yeezy-na-izi <howone1sky@gmail.com>
2022-10-23 11:48:06 +0300

    Update user admin page

--- users/admin.py
+++ users/admin.py
@@ -5,10 +5,84 @@ from .models import CustomUser
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (
+            "Конфиденциальная информация",
+            {
+                "fields": (
+                    "email",
+                    "password",
+                )
+            },
+        ),
+        (
+            "Персональная информация",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "patronymic",
+                    "birthday",
+                    "avatar",
+                )
+            },
+        ),
+        (
+            "Дополнительная информация",
+            {
+                "fields": (
+                    "about_me",
+                    "key_skills",
+                    "useful_to_project",
+                    "status",
+                    "speciality",
+                    "city",
+                    "region",
+                    "organization",
+                    "tags",
+                )
+            },
+        ),
+        (
+            "Права доступа",
+            {
+                "fields": (
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (
+            "Важные даты",
+            {
+                "fields": (
+                    "last_login",
+                    "date_joined",
+                )
+            },
+        ),
+    )
+
     list_display = (
         "email",
+        "last_name",
+        "first_name",
         "id",
     )
+    list_display_links = (
+        "email",
+        "first_name",
+        "last_name",
+    )
+
+    search_fields = (
+        "email",
+        "first_name",
+        "last_name",
+    )
+
     list_filter = (
         "email",
         "id",
