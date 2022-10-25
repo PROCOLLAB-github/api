d6bd3f2392245a88eab0b9c9258d121bc1755070
Mikhail Khromov <khromov05@gmail.com>
2022-10-25 21:22:34 +0300

    missing docstrings added

--- projects/filters.py
+++ projects/filters.py
@@ -9,17 +9,22 @@ class ProjectFilter(filters.FilterSet):
 
     ?industry=1 - equal to .filter(industry=1)
     ?step=1 - equal to .filter(step=1)
+    ?region=Moscow - equal to .filter(region='Moscow')
     ?name__contains=clown - equal to .filter(name__contains='clown')
+    ?description__contains=clown - equal to .filter(description__contains=='clown')
     ?collaborators__in=1,2,3 - equal to .filter(collaborators__in=[1, 2, 3])
     ?datetime_created__gt=01.01.2023 - equal to .filter(datetime_created__gt=01.01.2023)
     """
 
-    name__contains = filters.Filter(field_name='name', lookup_expr='contains')
-    description__contains = filters.Filter(field_name='name', lookup_expr='contains')
-    region = filters.CharFilter(field_name='region')
-    collaborators__in = filters.BaseInFilter(field_name='collaborators')
-    datetime_created__gt = filters.DateTimeFilter(field_name='datetime_created', lookup_expr='gt')
+    name__contains = filters.Filter(field_name="name", lookup_expr="contains")
+    description__contains = filters.Filter(
+        field_name="description", lookup_expr="contains"
+    )
+    collaborators__in = filters.BaseInFilter(field_name="collaborators")
+    datetime_created__gt = filters.DateTimeFilter(
+        field_name="datetime_created", lookup_expr="gt"
+    )
 
     class Meta:
         model = Project
-        fields = ('industry', 'step')
+        fields = ("industry", "step", "region")
