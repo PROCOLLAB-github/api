68cc9973fda8ab4809c45f852ff6c2b728c91fc7
Yakser <iaksanov2044@mail.ru>
2022-10-31 12:26:58 +0300

    Installed uvicorn

--- procollab/settings.py
+++ procollab/settings.py
@@ -179,10 +179,6 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-STATICFILES_FINDERS = [
-    "django.contrib.staticfiles.finders.FileSystemFinder",
-    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
-]
 
 
 # Default primary key field type
