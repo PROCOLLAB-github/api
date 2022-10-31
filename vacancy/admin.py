63c92bca573f01dd8895ba20eecadf96479c1c47
Mikhail Khromov <khromov05@gmail.com>
2022-10-31 21:40:16 +0300

    VacancyResponse admin

--- vacancy/admin.py
+++ vacancy/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from vacancy.models import Vacancy
+from vacancy.models import Vacancy, VacancyResponse
 
 
 @admin.register(Vacancy)
@@ -15,3 +15,15 @@ class VacancyAdmin(admin.ModelAdmin):
         "datetime_updated",
     ]
     list_display_links = ["role"]
+
+
+@admin.register(VacancyResponse)
+class VacancyResponseAdmin(admin.ModelAdmin):
+    list_display = [
+        "user",
+        "vacancy",
+        "why_me",
+        "is_approved",
+        "datetime_created",
+        "datetime_updated",
+    ]
