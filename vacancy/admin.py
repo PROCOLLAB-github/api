ef5393e809117917455878cdf52977a55290336d
Mikhail Khromov <khromov05@gmail.com>
2022-10-31 20:27:54 +0300

    админка для вакансий + ачивок

--- vacancy/admin.py
+++ vacancy/admin.py
@@ -1,1 +1,17 @@
-# Register your models here.
+from django.contrib import admin
+
+from vacancy.models import Vacancy
+
+
+@admin.register(Vacancy)
+class VacancyAdmin(admin.ModelAdmin):
+    list_display = [
+        "role",
+        "required_skills",
+        "description",
+        "project",
+        "is_active",
+        "datetime_created",
+        "datetime_updated",
+    ]
+    list_display_links = ["role"]
