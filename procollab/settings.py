d6254b8ae9f02a6ef83fa275cf3356fa66f02282
yeezy-na-izi <howone1sky@gmail.com>
2022-11-01 12:50:16 +0300

    Static fixed

--- procollab/settings.py
+++ procollab/settings.py
@@ -1,5 +1,4 @@
 import mimetypes
-import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -66,6 +65,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -180,8 +180,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 
