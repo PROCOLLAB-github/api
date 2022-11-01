8c7967dbd99c8a81edf47ad9f03ae7a387400979
Yakser <iaksanov2044@mail.ru>
2022-11-01 22:28:44 +0300

    Display specific user type fields

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 
-from .models import CustomUser
+from .models import CustomUser, Member, Mentor, Expert, Investor
 
 
 class UserSerializer(serializers.ModelSerializer):
+    user_type_fields = serializers.SerializerMethodField()
+
     class Meta:
         model = CustomUser
         fields = [
@@ -17,7 +19,31 @@ class UserSerializer(serializers.ModelSerializer):
             "city",
             "password",
             "is_active",
+            "user_type_fields",
         ]
+
+    def get_user_type_fields(self, obj):
+        # maybe thats not the best way to do it, but it also works
+
+        # user_type_to_serializer = {
+        #     "member": MemberSerializer,
+        #     "expert": ExpertSerializer,
+        #     "investor": InvestorSerializer,
+        #     "mentor": MentorSerializer,
+        # }
+        # for user_type in user_type_to_serializer.keys():
+        #     if hasattr(obj, user_type):
+        #         serializer = user_type_to_serializer[user_type](getattr(obj, user_type))
+        #         return serializer.data
+
+        if obj.user_type == CustomUser.MEMBER:
+            return MemberSerializer(obj.member).data
+        elif obj.user_type == CustomUser.MENTOR:
+            return MentorSerializer(obj.mentor).data
+        elif obj.user_type == CustomUser.EXPERT:
+            return ExpertSerializer(obj.expert).data
+        elif obj.user_type == CustomUser.INVESTOR:
+            return InvestorSerializer(obj.investor).data
 
     def create(self, validated_data):
         user = CustomUser(**validated_data)
@@ -25,6 +51,43 @@ class UserSerializer(serializers.ModelSerializer):
         user.save()
 
         return user
+
+
+class MemberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Member
+        fields = [
+            "key_skills",
+            "useful_to_project",
+            "speciality",
+        ]
+
+
+class MentorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Mentor
+        fields = [
+            "job",
+            "useful_to_project",
+        ]
+
+
+class ExpertSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Expert
+        fields = [
+            "preferred_industries",
+            "useful_to_project",
+        ]
+
+
+class InvestorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Investor
+        fields = [
+            "interaction_process_description",
+            "preferred_industries",
+        ]
 
 
 class EmailSerializer(serializers.Serializer):
