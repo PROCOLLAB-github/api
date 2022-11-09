e7b14e5a5482ddde28f1b1a518aa72e4e8e4372e
yeezy-na-izi <howone1sky@gmail.com>
2022-11-09 15:02:33 +0300

    Pylint

--- invites/managers.py
+++ invites/managers.py
@@ -3,8 +3,4 @@ from django.db.models import Manager
 
 class InviteManager(Manager):
     def get_invite_for_list_view(self):
-        return (
-            self.get_queryset()
-            .select_related("project", "user")
-        )
-
+        return self.get_queryset().select_related("project", "user")
