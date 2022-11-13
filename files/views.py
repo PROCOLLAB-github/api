e73ee288988bd6fc4d168d987d0d6faad153ff1c
Yakser <iaksanov2044@mail.ru>
2022-11-13 12:33:52 +0300

    Catch error when file is not passed

--- files/views.py
+++ files/views.py
@@ -2,7 +2,7 @@ from django.db import transaction
 from rest_framework import permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
+from django.utils.datastructures import MultiValueDictKeyError
 from files.helpers import FileAPI
 from files.models import UserFile
 
@@ -12,11 +12,13 @@ class FileView(APIView):
 
     @transaction.atomic
     def post(self, request):
-        file_api = FileAPI(request.FILES["file"], request.user)
-        status_code, url = file_api.upload()
+        try:
+            file_api = FileAPI(request.FILES["file"], request.user)
+            status_code, url = file_api.upload()
 
-        if status_code == 201:
-            UserFile.objects.create(user=request.user, link=url)
-            return Response({"url": url}, status=status.HTTP_201_CREATED)
-
-        return Response("Failed to upload file", status=status.HTTP_409_CONFLICT)
+            if status_code == 201:
+                UserFile.objects.create(user=request.user, link=url)
+                return Response({"url": url}, status=status.HTTP_201_CREATED)
+            return Response("Failed to upload file", status=status.HTTP_409_CONFLICT)
+        except MultiValueDictKeyError:
+            return Response("File is not passed", status=status.HTTP_400_BAD_REQUEST)
