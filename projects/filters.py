955189eebb56ef7407a189b84fc5927d0f56100a
Mikhail Khromov <khromov05@gmail.com>
2022-11-16 16:43:41 +0300

    fixed collaborators__in

--- projects/filters.py
+++ projects/filters.py
@@ -10,7 +10,7 @@ class ProjectFilter(filters.FilterSet):
 
     Parameters to filter by:
         industry (int), step (int), region (str), name__contains (str),
-         description__contains (str), collaborators__in (List[int]),
+         description__contains (str), collaborator__user__in (List[int]),
          datetime_created__gt (datetime.datetime)
 
     Examples:
@@ -18,18 +18,20 @@ class ProjectFilter(filters.FilterSet):
             equals to .filter(industry=1, name__contains='clown')
         ?datetime_created__gt=25.10.2022
             equals to .filter(datetime_created__gt=datetime.datetime(...))
-        ?collaborators__in=1,2 equals to .filter(collaborators__in=[1, 2])
+        ?collaborator__user__in=1,2 equals to .filter(collaborator__user__in=[1, 2])
     """
 
     name__contains = filters.Filter(field_name="name", lookup_expr="contains")
     description__contains = filters.Filter(
         field_name="description", lookup_expr="contains"
     )
-    collaborators__in = filters.BaseInFilter(field_name="collaborators")
+    collaborator__user__in = filters.BaseInFilter(
+        field_name="collaborator__user", lookup_expr="in"
+    )
     datetime_created__gt = filters.DateTimeFilter(
         field_name="datetime_created", lookup_expr="gt"
     )
 
     class Meta:
         model = Project
-        fields = ("industry", "step", "region")
+        fields = ("industry", "step", "region", "leader")
