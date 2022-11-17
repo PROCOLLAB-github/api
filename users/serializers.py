69634375dc32cf80bf2b9d041547bbe4e1bb4309
Yakser <iaksanov2044@mail.ru>
2022-11-17 16:17:30 +0300

    Add birthday field to UserDetailSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -68,6 +68,7 @@ class UserDetailSerializer(serializers.ModelSerializer):
             "last_name",
             "patronymic",
             "speciality",
+            "birthday",
             "avatar",
             "city",
             "is_active",
@@ -109,13 +110,6 @@ class UserDetailSerializer(serializers.ModelSerializer):
                 continue
             setattr(instance, attr, value)
 
-        # if "achievements" in validated_data:
-        #     instance.achievements.all().delete()
-        #     instance.achievements.clear()
-        #     for achievement in validated_data["achievements"]:
-        #         obj = UserAchievement(**achievement)
-        #         obj.save()
-        #         instance.achievements.add(obj)
         instance.save()
 
         return instance
