d7add126d0864fa3ae44c52ced4a74e1063a1415
Mikhail Khromov <khromov05@gmail.com>
2022-11-28 05:26:55 +0300

    added project filters by member count (gt & lt), step, vacancy existence

--- projects/filters.py
+++ projects/filters.py
@@ -11,7 +11,8 @@ class ProjectFilter(filters.FilterSet):
     Parameters to filter by:
         industry (int), step (int), region (str), name__contains (str),
          description__contains (str), collaborator__user__in (List[int]),
-         datetime_created__gt (datetime.datetime)
+         datetime_created__gt (datetime.datetime), step (int), any_vacancies (bool),
+         member_count__gt (int), member_count__lt (int), leader (int)
 
     Examples:
         ?industry=1&name__contains=clown
@@ -19,6 +20,9 @@ class ProjectFilter(filters.FilterSet):
         ?datetime_created__gt=25.10.2022
             equals to .filter(datetime_created__gt=datetime.datetime(...))
         ?collaborator__user__in=1,2 equals to .filter(collaborator__user__in=[1, 2])
+        ?step=1 equals to .filter(step=1)
+        ?any_vacancies=true equals to .filter(any_vacancies=True)
+        ?member_count__gt=1 equals to .filter(member_count__gt=1)
     """
 
     name__contains = filters.Filter(field_name="name", lookup_expr="contains")
@@ -32,6 +36,17 @@ class ProjectFilter(filters.FilterSet):
         field_name="datetime_created", lookup_expr="gt"
     )
 
+    # filters by whether there are any vacancies in the project
+    any_vacancies = filters.BooleanFilter(
+        field_name="vacancy", lookup_expr="isnull", exclude=True
+    )
+    member_count_gt = filters.NumberFilter(
+        field_name="collaborator", lookup_expr="count__gt"
+    )
+    member_count_lt = filters.NumberFilter(
+        field_name="collaborator", lookup_expr="count__lt"
+    )
+
     class Meta:
         model = Project
-        fields = ("industry", "step", "region", "leader")
+        fields = ("industry", "step", "region", "leader", "step")
