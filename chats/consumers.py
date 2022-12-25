9904ce91664b2bc69be0b0b407c7e382c5463154
Mikhail Khromov <khromov05@gmail.com>
2022-12-25 19:42:41 +0300

    authorization for consumers

--- chats/consumers.py
+++ chats/consumers.py
@@ -5,8 +5,17 @@ from channels.generic.websocket import JsonWebsocketConsumer
 
 
 class ChatConsumer(JsonWebsocketConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
+        self.room_name = None
+        self.user = None
+
     def connect(self):
-        # TODO 1. check that user is authenticated
+        self.user = self.scope["user"]
+        # check that user is authenticated
+        if not self.user.is_authenticated:
+            return
+
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
 
@@ -25,12 +34,31 @@ class ChatConsumer(JsonWebsocketConsumer):
 
     # Receive message from WebSocket
     def receive_json(self, content, **kwargs):
-        message = content["message"]
+        message_type = content["type"]
 
-        # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name, {"type": "chat_message", "message": message}
-        )
+        if message_type == "chat_message":
+            # TODO
+            # message = Message.objects.create(
+            #     author=self.user,
+            #     content=content["message"],
+            #     chat=
+            # )
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_name,
+                {
+                    "type": "chat_message_echo",
+                    "name": content["name"],
+                    "message": content["message"],
+                },
+            )
+        elif message_type == "typing":
+            # TODO
+            pass
+        elif message_type == "read":
+            # TODO
+            pass
+
+        return super().receive_json(content, **kwargs)
 
     # Receive message from room group
     def chat_message(self, event):
@@ -38,3 +66,18 @@ class ChatConsumer(JsonWebsocketConsumer):
 
         # Send message to WebSocket
         self.send(text_data=json.dumps({"message": message}))
+
+
+class NotificationConsumer(JsonWebsocketConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
+        self.user = None
+
+    def connect(self):
+        self.user = self.scope["user"]
+        if not self.user.is_authenticated:
+            return
+
+        self.accept()
+
+        # Send count of unread messages
