02177d1ef07688ebac3f22d9d0e2a0f8257863a9
Yakser <iaksanov2044@mail.ru>
2023-01-23 21:19:27 +0400

    Fixed ProjectChat creation

--- projects/apps.py
+++ projects/apps.py
@@ -5,3 +5,6 @@ class ProjectsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "projects"
     verbose_name = "Проекты"
+
+    def ready(self):
+        import projects.signals  # noqa: F401
