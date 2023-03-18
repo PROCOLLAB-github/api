2caefad8c3b6f11238f4fbb2c56a66cff4105293
afarut <renat030205@ya.ru>
2023-03-18 04:00:05 +0300

    mini refactoring

--- events/constants.py
+++ events/constants.py
@@ -1,10 +1,2 @@
-from django.conf import settings
-
-
 APP_URL = "https://procollab.ru/events"
-EDIT_MESSAGE_URL = (
-    f"https://api.telegram.org/bot{settings.TELEGRAM_BOT_TOKEN}/editMessageText"
-)
-SEND_MESSAGE_URL = (
-    f"https://api.telegram.org/bot{settings.TELEGRAM_BOT_TOKEN}/sendMessage"
-)
+TELEGRAM_API_URL = "https://api.telegram.org/bot"
--- events/helpers.py
+++ events/helpers.py
@@ -1,22 +1,24 @@
 import requests
-from events.constants import EDIT_MESSAGE_URL
-from events.constants import SEND_MESSAGE_URL
+from events.constants import TELEGRAM_API_URL
+from django.conf import settings
 
 
 def send_message(text, chat_id):
+    url = TELEGRAM_API_URL + settings.TELEGRAM_BOT_TOKEN + "/sendMessage"
     data = {
         "chat_id": chat_id,
         "text": text,
         "parse_mode": "markdown",
     }
-    return requests.post(SEND_MESSAGE_URL, data=data).json()
+    return requests.post(url, data=data).json()
 
 
 def edit_message(text, message_id, chat_id):
+    url = TELEGRAM_API_URL + settings.TELEGRAM_BOT_TOKEN + "/editMessageText"
     data = {
         "chat_id": chat_id,
         "text": text,
         "parse_mode": "markdown",
         "message_id": message_id,
     }
-    return requests.post(EDIT_MESSAGE_URL, data=data).json()
+    return requests.post(url, data=data).json()
