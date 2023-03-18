922b80a5b48974d6fc8d56b90bcf0dd595d6a9ac
stolzor <ruslangataullin16@gmail.com>
2023-03-18 11:35:14 +0300

    add new test for invites

--- invites/tests.py
+++ invites/tests.py
@@ -7,6 +7,7 @@ from tests.constants import USER_CREATE_DATA
 from users.views import UserList
 from users.models import CustomUser
 from invites.views import InviteList, InviteDetail
+from invites.models import Invite
 from industries.models import Industry
 from projects.views import ProjectList, ProjectDetail
 
@@ -52,6 +53,67 @@ class InvitesTestCase(TestCase):
         response = self.invite_list_view(request)
 
         self.assertEqual(response.status_code, 201)
+        self.assertEqual(
+            response.data["motivational_letter"],
+            self.invite_create_data["motivational_letter"],
+        )
+        self.assertEqual(
+            response.data["project"]["id"], self.invite_create_data["project"]
+        )
+        self.assertEqual(response.data["role"], self.invite_create_data["role"])
+        self.assertEqual(
+            response.data["is_accepted"], self.invite_create_data["is_accepted"]
+        )
+
+    def test_invites_creation_with_empty_text(self):
+        user_main = self._user_create("example@gmail.com")
+        user2 = self._user_create("example2@gmail.com")
+        project = self._project_create(user_main)
+
+        self.invite_create_data["user"] = user2.id
+        self.invite_create_data["project"] = project.id
+        self.invite_create_data["motivational_letter"] = None
+        request = self.factory.post("invites/", self.invite_create_data, format="json")
+        force_authenticate(request, user=user_main)
+
+        response = self.invite_list_view(request)
+
+        self.assertEqual(response.status_code, 201)
+
+    def test_invites_update(self):
+        user_main = self._user_create("example@gmail.com")
+        user2 = self._user_create("example2@gmail.com")
+        project = self._project_create(user_main)
+
+        self.invite_create_data["user"] = user2.id
+        self.invite_create_data["project"] = project.id
+        request = self.factory.post("invites/", self.invite_create_data, format="json")
+        force_authenticate(request, user=user_main)
+
+        response = self.invite_list_view(request)
+
+        invite_id = response.data["id"]
+        invite = Invite.objects.get(id=invite_id)
+
+        request = self.factory.patch(
+            f"invites/{invite.pk}/", {"motivational_letter": "HELLO GUYS!"}
+        )
+        force_authenticate(request, user=user_main)
+        response = self.invite_detail_view(request, pk=invite.pk)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["motivational_letter"], "HELLO GUYS!")
+
+    def test_invites_creation_with_empty_data(self):
+        user_main = self._user_create("example@gmail.com")
+
+        self.invite_create_data = {}
+
+        request = self.factory.post("invites/", self.invite_create_data, format="json")
+        force_authenticate(request, user=user_main)
+        response = self.invite_list_view(request)
+
+        self.assertEqual(response.status_code, 400)
 
     def _project_create(self, user):
         request = self.factory.post("projects/", self.project_create_data)
@@ -62,7 +124,7 @@ class InvitesTestCase(TestCase):
         return Project.objects.get(pk=project_id)
 
     def _user_create(self, email):
-        tmp_create_data = USER_CREATE_DATA
+        tmp_create_data = USER_CREATE_DATA.copy()
         tmp_create_data["email"] = email
         request = self.factory.post("auth/users/", tmp_create_data)
         response = self.user_list_view(request)
