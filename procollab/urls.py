2ac0ac5b19af3db389ee192aa8d872de78ce4d6b
Pichcher <chernov.pichcher@gmail.com>
2023-04-03 15:57:12 +0300

    in procollab/url change permissions because we had one problem - everyone can check our requests, so I canched AllowAny to IsStaffOrReadOnly(from core/pemissions) and delete base permissions (from rest_framework import permissions) because flake

--- procollab/urls.py
+++ procollab/urls.py
@@ -4,7 +4,6 @@ from django.contrib import admin
 from django.urls import include, path, re_path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
-from rest_framework import permissions
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
