09ea2db96506cf22b856550ba445dbed5592ab98
XataBq <excist97@mail.ru>
2023-04-12 14:23:10 +0300

    added url events/types/

--- events/urls.py
+++ events/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from events.views import EventsList, EventDetail, EventRegisteredUsersList
+from events.views import EventsList, EventDetail, EventRegisteredUsersList, EventTypes
 
 app_name = "events"
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path("", EventsList.as_view()),
     path("<int:pk>/", EventDetail.as_view()),
     path("<int:pk>/registered/", EventRegisteredUsersList.as_view()),
+    path("types/", EventTypes.as_view()),
 ]
