a17657f21458388f38b0117b5465e531ea81c70d
Yakser <iaksanov2044@mail.ru>
2023-05-01 14:05:22 +0300

    Fixed indent in `VerificationTasks` class

--- users/services/verification.py
+++ users/services/verification.py
@@ -58,8 +58,7 @@ class VerificationTasks:
             # fixme
             pass
 
-
-def __new__(cls, *args, **kwargs):
-    if not hasattr(cls, "instance"):
-        cls.instance = super().__new__(cls)
-    return cls.instance
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, "instance"):
+            cls.instance = super().__new__(cls)
+        return cls.instance
