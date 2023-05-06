d61f0be019bbed9943794680d4fb42d89e840686
Yakser <iaksanov2044@mail.ru>
2023-05-06 17:11:09 +0300

    Send email when user verification is completed

--- users/helpers.py
+++ users/helpers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.sites.shortcuts import get_current_site
 from django.urls import reverse
@@ -5,6 +6,8 @@ from django.urls import reverse
 from core.utils import Email
 from users.constants import PROTOCOL
 from users.models import UserAchievement
+
+User = get_user_model()
 
 
 def reset_email(user, request):
@@ -59,6 +62,29 @@ def verify_email(user, request):
     data = {
         "email_body": email_body,
         "email_subject": "Procollab | Подтверждение почты",
+        "to_email": user.email,
+    }
+
+    Email.send_email(data)
+
+
+def send_verification_completed_email(user: User):
+    # fixme
+    email_body = (
+        f"Поздравляю тебя, {user.first_name} {user.last_name}! Ты прошел верификацию и"
+        f" стал частью сообщества PROCOLLAB!"
+        f"Теперь ты сможешь пользоваться всем функционалом платформы, создавать проекты,"
+        f" искать команду, находить нужные мероприятия."
+        f"Следи за анонсами обновлений в нашей группе в ВК https://vk.com/PROCOLLAB "
+        f"И скорее переходи на саму платформу, чтобы уже сегодня начать создавать свой проект."
+        f"https://procollab.ru "
+        f"С уважением, "
+        f"Администрация PROCOLLAB"
+    )
+
+    data = {
+        "email_body": email_body,
+        "email_subject": "Procollab | Верификация",
         "to_email": user.email,
     }
 
