8fc2fd895350bbca0d5b3c44acbdd9302d6fb24e
denkoten <kotenko341@gmail.com>
2023-05-14 23:19:06 +0300

    Add test_change_user_type_without_preferred_industries_from_member_to_mentor

--- users/tests.py
+++ users/tests.py
@@ -117,3 +117,20 @@ class UserTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         user.refresh_from_db()
         self.assertEqual(user.user_type, user.user_type)
+
+    def test_change_user_type_without_preferred_industries_from_member_to_mentor(self):
+        request = self.factory.post("auth/users/", USER_CREATE_DATA)
+        response = self.user_list_view(request)
+        user_id = response.data["id"]
+        user = CustomUser.objects.get(id=user_id)
+
+        new_user_type = CustomUser.MENTOR
+        request = self.factory.patch(
+            f"auth/users/{user.pk}/", {"user_type": new_user_type}
+        )
+        force_authenticate(request, user=user)
+        response = self.user_detail_view(request, pk=user.pk)
+
+        self.assertEqual(response.status_code, 200)
+        user.refresh_from_db()
+        self.assertEqual(user.user_type, user.user_type)
