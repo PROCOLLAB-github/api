2b3a493215f9ae2083aaf6000e74910dc4348a08
afarut <renat030205@ya.ru>
2023-09-09 22:56:07 +0300

    commit for merge

---
+++ core/log/utils.py
@@ -0,0 +1,18 @@
+import logging
+import sys
+from loguru import logger
+
+
+class InterceptHandler(logging.Handler):
+    def emit(self, record):
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        frame, depth = sys._getframe(6), 6
+        while frame and frame.f_code.co_filename == logging.__file__:
+            frame = frame.f_back
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
