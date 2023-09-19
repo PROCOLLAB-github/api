697393fe90bba57744a42fb87817ed7bed6c8144
Koshak <kokolen77@gmail.com>
2023-09-19 20:28:02 +0300

    remove template in root

--- partner_programs/admin.py
+++ partner_programs/admin.py
@@ -1,7 +1,4 @@
-from collections import defaultdict
-
 import tablib
-from django.template.response import TemplateResponse
 
 from django.contrib import admin
 from django.http import HttpResponse
@@ -44,7 +41,7 @@ class PartnerProgramUserProfileAdmin(admin.ModelAdmin):
         custom_urls = [
             re_path(
                 "export/$",
-                self.admin_site.admin_view(self.choise_fileds_to_export),
+                self.admin_site.admin_view(self.get_export_file),
                 name="export_partners_programs",
             ),
             re_path(
@@ -55,87 +52,53 @@ class PartnerProgramUserProfileAdmin(admin.ModelAdmin):
         ]
         return custom_urls + urls
 
-    def choise_fileds_to_export(self, request):
-        # Тут подтягивать из бд
-        available_fields = [
-            ["Имя", "first_name"],
-            ["Фамилия", "last_name"],
-            ["Отчество", "patronymic"],
-            ["Почта", "email"],
-            ["Номер телефона", "phone_number"],
-            ["Дата рождения", "birthday"],
-            ["Тип населенного пункта", "type_locality"],
-            ["Регион", "region"],
-            ["Город", "city"],
-            ["ФИО родителя", "parent_name"],
-            ["Почта родителя", "parent_email"],
-            ["Номер родителя", "parent_phone"],
-            ["Название проекта", "project_name"],
-            ["Презентация проекта", "project_presentation_address"],
-            ["Число участников", "member_count"],
-            ["Наличие наставника", "availability_mentor"],
-            ["Место работы и должность наставника", "work_mentor_", "work_mentor2"],
-        ]
-
-        context = []
-
-        for index, i in enumerate(available_fields):
-            context.append(
-                {
-                    "column_name": i[0],
-                    "json_keys": ";".join(i[1:]),
-                    "id": index,
-                }
-            )
-
-        context = {"fields_to_export": context}
-        context = super(PartnerProgramUserProfileAdmin, self).changelist_view(
-            request, context
-        )
-        context = context.context_data
-
-        return TemplateResponse(
-            request, "admin/partner_programs/fields_choice.html", context
-        )
-
     def get_export_file(self, request):
-        column_names = []
-        column_keys = defaultdict(lambda: [])
-
-        for i in request.POST:
-            try:
-                key, index = i.split("-")
-                index = int(index)
-            except Exception:
-                continue
-            if key == "column_name":
-                if index >= len(column_names):
-                    column_names.append(request.POST[i])
-            elif key == "column_key":
-                column_name = column_names[index]
-                for column_key in request.POST[i].split(";"):
-                    if column_key == "":
-                        continue
-                    column_keys[column_name].append(column_key)
-        response_data = tablib.Dataset(headers=column_names)
+        column_order = [
+            "Имя",
+            "Фамилия",
+            "Отчество",
+            "Почта",
+            "Номер телефона",
+            "Дата рождения",
+            "Тип населенного пункта",
+            "Регион",
+            "Город",
+            "ФИО родителя",
+            "Почта родителя",
+            "Номер родителя",
+            "Название проекта",
+            "Презентация проекта",
+            "Число участников",
+            "Наличие наставника",
+            "Место работы и должность наставника",
+        ]
+        response_data = tablib.Dataset(headers=column_order)
 
         all_profiles = PartnerProgramUserProfile.objects.all()
 
         for profile in all_profiles:
+            schema = profile.partner_program.data_schema
+            keys_in_scheme = {}
+            for key in schema:
+                keys_in_scheme[schema[key]["name"]] = key
             json_data = profile.partner_program_data
             row = []
-            json_data["first_name"] = profile.user.first_name
-            json_data["last_name"] = profile.user.last_name
-            json_data["patronymic"] = profile.user.patronymic
-            json_data["email"] = profile.user.email
+            row.extend(
+                [
+                    profile.user.first_name,
+                    profile.user.last_name,
+                    profile.user.patronymic,
+                    profile.user.email,
+                ]
+            )
             json_data["birthday"] = str(profile.user.birthday)
-            for column_name in column_names:
-
+            keys_in_scheme["День рождения"] = str(profile.user.birthday)
+            for column_name in column_order[4:]:
                 current_value = ""
-                for json_key in column_keys[column_name]:
-                    if json_key in json_data:
-                        current_value = json_data[json_key]
-                        break
+                if column_name in keys_in_scheme:
+                    key = keys_in_scheme[column_name]
+                    if key in json_data:
+                        current_value = json_data[key]
                 row.append(current_value)
             response_data.append(row)
 
