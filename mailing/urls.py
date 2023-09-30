cf5e8e65c958568e6cc5a9321ea823f33439896d
Koshak <kokolen77@gmail.com>
2023-09-30 15:11:03 +0300

    urlpatterns fix

--- mailing/urls.py
+++ mailing/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from mailing.views import some_task, MailingSchemaView, template_fields, SendMailView
+from mailing.views import MailingSchemaView, template_fields, SendMailView
 
 app_name = "mailing"
 
 urlpatterns = [
-    path("", some_task),
     path("mail", MailingSchemaView.as_view()),
     path("template_fileds/<int:schema_id>/", template_fields, name="template_fields"),
     path("send_email", SendMailView.as_view(), name="send_mail"),
