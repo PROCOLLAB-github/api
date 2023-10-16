28596203231ce8e8d24afe5c442a7e2f8d5015ae
MaskedMir <maratbeibutov@ya.ru>
2023-10-16 18:22:15 +0300

    Resize image

--- files/helpers.py
+++ files/helpers.py
@@ -7,3 +7,33 @@ def convert_image_to_webp(image, quality: int = 70):
     pil_image = Image.open(image.file)
     webp_image = webp.WebPPicture.from_pil(pil_image)
     return webp_image.encode(config)
+
+
+def resize_image(image, size=(512, 512)):
+    pil_image = Image.open(image.file)
+
+    if (
+        pil_image.height < pil_image.width
+    ):  # Подгонка параметров для горизонтальных изображений
+        width, height = pil_image.width, pil_image.width
+        x, y = 0, int((pil_image.height - height) // 2)
+
+    elif (
+        pil_image.height > pil_image.width
+    ):  # Подгонка параметров для вертикальных изображений
+        width, height = pil_image.height, pil_image.height
+        x, y = int((pil_image.width - width) // 2), 0
+
+    else:
+        width, height = (
+            pil_image.width,
+            pil_image.height,
+        )  # Подгонка параметров для квадратных изображений
+        x, y = 0, 0
+
+    area = (x, y, x + width, y + height)  # Итоговые размеры до ресайза
+
+    pil_image = pil_image.crop(area)
+    pil_image = pil_image.resize(size)
+
+    return pil_image
