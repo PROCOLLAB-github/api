f946d76ffed213a7fb3be2eaff0cc610d809c9c8
Mikhail Khromov <khromov05@gmail.com>
2023-11-08 17:47:38 +0300

    work on news for users

--- news/mixins.py
+++ news/mixins.py
@@ -1,6 +1,10 @@
+from django.contrib.auth import get_user_model
+
 from news.models import News
 from partner_programs.models import PartnerProgram
 from projects.models import Project
+
+User = get_user_model()
 
 
 class NewsQuerysetMixin:
@@ -9,18 +13,31 @@ class NewsQuerysetMixin:
     """
 
     def get_queryset_for_project(self):
+        project_id = self.kwargs.get("project_pk")
         try:
-            project = Project.objects.get(pk=self.kwargs.get("project_pk"))
+            project = Project.objects.get(pk=project_id)
         except Project.DoesNotExist:
+            # TODO: raise http 404 here
             return News.objects.none()
         return News.objects.get_news(obj=project)
 
     def get_queryset_for_program(self):
+        partner_program_id = self.kwargs.get("partnerprogram_pk")
         try:
-            program = PartnerProgram.objects.get(pk=self.kwargs.get("partnerprogram_pk"))
+            program = PartnerProgram.objects.get(pk=partner_program_id)
         except PartnerProgram.DoesNotExist:
+            # TODO: raise http 404 here
             return News.objects.none()
         return News.objects.get_news(obj=program)
+
+    def get_queryset_for_user(self):
+        user_id = self.kwargs.get("user_pk")
+        try:
+            user = User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            # TODO: raise http 404 here
+            return News.objects.none()
+        return News.objects.get_news(obj=user)
 
     def get_queryset(self):
         if self.kwargs.get("project_pk") is not None:
@@ -29,5 +46,8 @@ class NewsQuerysetMixin:
         elif self.kwargs.get("partnerprogram_pk") is not None:
             # it's a partner program
             return self.get_queryset_for_program()
+        elif self.kwargs.get("user_pk") is not None:
+            # it's a user news
+            return self.get_queryset_for_user()
         else:
             return News.objects.none()
--- news/views.py
+++ news/views.py
@@ -1,15 +1,19 @@
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
 from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from core.serializers import SetViewedSerializer, SetLikedSerializer
 from core.services import add_view, set_like
+from news.mixins import NewsQuerysetMixin
 from news.models import News
 from news.pagination import NewsPagination
 from news.permissions import IsNewsCreatorOrReadOnly
 from news.serializers import NewsListSerializer, NewsDetailSerializer
-from news.mixins import NewsQuerysetMixin
 from projects.models import Project
+
+User = get_user_model()
 
 
 class NewsList(NewsQuerysetMixin, generics.ListCreateAPIView):
@@ -19,14 +23,21 @@ class NewsList(NewsQuerysetMixin, generics.ListCreateAPIView):
 
     def post(self, request, *args, **kwargs):
         if kwargs.get("project_pk"):
-            project = Project.objects.get(pk=kwargs["project_pk"])
+            project = get_object_or_404(Project, pk=kwargs["project_pk"])
             news = News.objects.add_news(project, **request.data)
             return Response(
                 NewsDetailSerializer(news).data, status=status.HTTP_201_CREATED
             )
+        elif kwargs.get("user_pk"):
+            # creating user news
+            user = get_object_or_404(User, pk=kwargs["user_pk"])
+            news = News.objects.add_news(user, **request.data)
+            return Response(
+                NewsDetailSerializer(news).data, status=status.HTTP_201_CREATED
+            )
         else:
-            # creating partner program news, not implemented yet
-            raise NotImplementedError()
+            # creating partner program news, not implemented yet, return 400
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
     def get(self, request, *args, **kwargs):
         news = self.paginate_queryset(self.get_queryset())
