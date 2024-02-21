e2fe20fcf03e0bf0490c8f199af781067b6ab017
Alexey Kudelko <flexivanov237@gmail.com>
2024-02-21 04:40:58 +0300

    fix serializer

--- rate_projects/serializers.py
+++ rate_projects/serializers.py
@@ -1,9 +1,27 @@
 from rest_framework import serializers
 
-from rate_projects.models import ProjectScore
+
+from rate_projects.models import ProjectScore, Criteria
+from rate_projects.validators import ProjectScoreValidate
 
 
 class ProjectScoreCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProjectScore
         fields = ["criteria_id", "user_id", "project_id", "value"]
+        readonly_field = ["criterion_field"]
+
+
+def serialize_data_func(criteria_to_get: list, data: dict):
+    criteria = Criteria.objects.in_bulk(criteria_to_get)
+
+    for criterion in data:
+        needed_criteria = criteria.get(int(criterion["criterion_id"]))
+
+        ProjectScoreValidate(
+            criteria_type=needed_criteria.type,
+            value=criterion["value"],
+            criteria_min_value=needed_criteria.min_value,
+            criteria_max_value=needed_criteria.max_value,
+        )
+        criterion["criteria_id"] = criterion.pop("criterion_id")
--- rate_projects/views.py
+++ rate_projects/views.py
@@ -4,7 +4,7 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 
 from rate_projects.models import ProjectScore
-from rate_projects.serializers import ProjectScoreCreateSerializer
+from rate_projects.serializers import ProjectScoreCreateSerializer, serialize_data_func
 from users.permissions import IsExpert
 
 User = get_user_model()
@@ -15,27 +15,27 @@ class RateProject(generics.CreateAPIView):
     permission_classes = [IsExpert]
 
     def create(self, request, *args, **kwargs):
-        try:
-            data = self.request.data
+        # try:
+        data = self.request.data
 
-            user = self.request.user.id
-            project_id = self.kwargs.get("project_id")
-            for criterion in data:
-                criterion["user_id"] = user
-                criterion["project_id"] = project_id
-                criterion["criteria_id"] = criterion.pop("criterion_id")
+        user = self.request.user.id
+        project_id = self.kwargs.get("project_id")
 
-            serializer = self.get_serializer(data=data, many=True)
-            if serializer.is_valid():
-                serializer.is_valid()
+        criteria_to_get = []
+        for criterion in data:
+            criterion["user_id"] = user
+            criterion["project_id"] = project_id
+            criteria_to_get.append(criterion["criterion_id"])
 
-                ProjectScore.objects.bulk_create(
-                    [ProjectScore(**score) for score in data]
-                )
+        serializer = self.get_serializer(data=data, many=True)
 
-                return Response({"success": True}, status=status.HTTP_201_CREATED)
+        if serializer.is_valid():
+            serialize_data_func(criteria_to_get, data)
+            ProjectScore.objects.bulk_create([ProjectScore(**score) for score in data])
 
-            else:
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        except Exception as e:
-            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"success": True}, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # except Exception as e:
+    #     return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
