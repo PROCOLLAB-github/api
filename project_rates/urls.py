88381c122bbc6d2a5e5b7c42eb0946912e13103f
Alexey Kudelko <flexivanov237@gmail.com>
2024-02-21 20:10:39 +0300

    get details added

--- project_rates/urls.py
+++ project_rates/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from project_rates.views import RateProject, RateProjects
+from project_rates.views import RateProject, RateProjects, RateProjectsDetails
 
 urlpatterns = [
     path("rate/", RateProject.as_view()),
     path("<int:program_id>", RateProjects.as_view()),
+    path("details/<int:project_id>", RateProjectsDetails.as_view()),
 ]
