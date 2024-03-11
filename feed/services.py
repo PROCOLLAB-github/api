c2d64bd2f833dc6bd4ec1a2aa23802bfb2168690
Alexey Kudelko <flexivanov237@gmail.com>
2024-03-11 11:52:38 +0300

    deleted useless variables x2

--- feed/services.py
+++ feed/services.py
@@ -13,6 +13,4 @@ def delete_news_for_model(instance: SIGNALS_MODELS):
 
 def create_news_for_model(instance: SIGNALS_MODELS):
     content_type = ContentType.objects.get_for_model(instance)
-    news_instance, created = News.objects.get_or_create(
-        content_type=content_type, object_id=instance.id
-    )
+    News.objects.get_or_create(content_type=content_type, object_id=instance.id)
