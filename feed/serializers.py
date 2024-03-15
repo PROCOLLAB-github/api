016cef1f83fdd421a170cb095aa8e084eea94891
Alexey Kudelko <flexivanov237@gmail.com>
2024-03-15 23:53:23 +0300

    optimized liked news in feed

--- feed/serializers.py
+++ feed/serializers.py
@@ -1,7 +1,68 @@
 from rest_framework import serializers
-from feed import constants
+
+from core.services import get_views_count, get_likes_count
+from feed.mapping import CONTENT_OBJECT_MAPPING, CONTENT_OBJECT_SERIALIZER_MAPPING
+from files.serializers import UserFileSerializer
+from news.mapping import NewsMapping
+from news.models import News
+from projects.models import Project
+from users.models import CustomUser
 
 
-class FeedItemSerializer(serializers.Serializer):
-    type_model = serializers.ChoiceField(choices=constants.FeedItemType, required=True)
-    content = serializers.JSONField(required=True)
+class NewsFeedListSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField()
+    image_address = serializers.SerializerMethodField()
+    is_user_liked = serializers.SerializerMethodField()
+    files = UserFileSerializer(many=True)
+    views_count = serializers.SerializerMethodField()
+    likes_count = serializers.SerializerMethodField()
+    content_object = serializers.SerializerMethodField()
+    type_model = serializers.SerializerMethodField()
+
+    def get_type_model(self, obj) -> str:
+        model_type = CONTENT_OBJECT_MAPPING[obj.content_type.model]
+        if obj.text != "" and model_type == "project":
+            return "news"
+        return model_type
+
+    def get_content_object(self, obj) -> dict:
+        type_model = obj.content_type.model
+        if obj.text != "" and self.get_type_model(obj) == "project":
+            type_model = "news"
+        serializer = CONTENT_OBJECT_SERIALIZER_MAPPING[type_model](obj.content_object)
+        return serializer.data
+
+    def get_views_count(self, obj):
+        return get_views_count(obj)
+
+    def get_likes_count(self, obj):
+        return get_likes_count(obj)
+
+    def get_name(self, obj):
+        if obj.content_type.model == CustomUser.__name__.lower():
+            return f"{obj.content_object.first_name} {obj.content_object.last_name}"
+        elif obj.text != "" and obj.content_type.model == Project.__name__.lower():
+            return f"{obj.content_object.name}"
+
+    def get_image_address(self, obj):
+        return NewsMapping.get_image_address(obj.content_object)
+
+    def get_is_user_liked(self, obj):
+        return obj.id in self.context.get("liked_news")
+
+    class Meta:
+        model = News
+        fields = [
+            "id",
+            "name",
+            "image_address",
+            "text",
+            "datetime_created",
+            "views_count",
+            "likes_count",
+            "files",
+            "is_user_liked",
+            "content_object",
+            "type_model",
+        ]
+        read_only_fields = ["views_count", "likes_count", "type_model"]
--- feed/services.py
+++ feed/services.py
@@ -1,9 +1,20 @@
 from django.contrib.contenttypes.models import ContentType
 
+from core.models import Like
 from feed.constants import SIGNALS_MODELS
 from news.models import News
+from users.models import CustomUser
 
 
+def get_liked_news(user: CustomUser, queryset: list[News]) -> list[int]:
+    obj_type = ContentType.objects.get_for_model(News)
+    liked_news = Like.objects.filter(
+        content_type=obj_type, object_id__in=[news.id for news in queryset], user=user
+    ).values_list("object_id", flat=True)
+    return liked_news
+
+
+# signals services
 def delete_news_for_model(instance: SIGNALS_MODELS):
     content_type = ContentType.objects.get_for_model(instance)
     obj = News.objects.filter(
