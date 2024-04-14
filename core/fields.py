19000646edbb4c626632d5d8924d6f35fc756964
DaniilSvinsov <svdadm164@gmail.com>
2024-04-14 22:54:54 +0300

    add type-hints core

--- core/fields.py
+++ core/fields.py
@@ -2,8 +2,8 @@ from rest_framework import serializers
 
 
 class CustomListField(serializers.ListField):
-    def to_representation(self, data):
+    def to_representation(self, data) -> list:
         return [value.strip() for value in data.split(",") if value.strip()]
 
-    def to_internal_value(self, data):
+    def to_internal_value(self, data) -> str:
         return ",".join(data)
