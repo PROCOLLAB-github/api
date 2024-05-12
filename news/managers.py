ee895a047a7867a84efc4a6e4b8b726a2f2de7dc
DaniilSvinsov <svdadm164@gmail.com>
2024-05-12 14:33:25 +0300

    add small fix type-hints in managers

--- news/managers.py
+++ news/managers.py
@@ -1,14 +1,15 @@
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from news.models import News
 
 
 class NewsManager(models.Manager):
     def get_news(self, obj: models.Model) -> models.QuerySet:
-        obj_type: ContentType = ContentType.objects.get_for_model(obj)
+        obj_type = ContentType.objects.get_for_model(obj)
         return self.get_queryset().filter(content_type=obj_type, object_id=obj.pk)
 
-    def add_news(self, obj: models.Model, **kwargs) -> models.Model:
-        obj_type: ContentType = ContentType.objects.get_for_model(obj)
+    def add_news(self, obj: models.Model, **kwargs) -> News:
+        obj_type = ContentType.objects.get_for_model(obj)
         kwargs = dict(kwargs)
         files = kwargs.pop("files", [])
         news = self.create(content_type=obj_type, object_id=obj.pk, **kwargs)
