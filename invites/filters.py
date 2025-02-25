d32f79ee30694db92404d334382630c22dba9c85
Sergey Yaksanov <iaksanov2044@mail.ru>
2025-02-25 22:06:11 +0300

    fix: filter invites by user_id field

--- invites/filters.py
+++ invites/filters.py
@@ -4,6 +4,14 @@ from invites.models import Invite
 from vacancy.filters import project_id_filter
 
 
+def user_id_filter(queryset, name, value):
+    return queryset.filter(
+        **{
+            "user_id": value[0],
+        }
+    )
+
+
 class InviteFilter(filters.FilterSet):
     """Filter for Invite
 
@@ -21,17 +29,20 @@ class InviteFilter(filters.FilterSet):
     def __init__(self, *args, **kwargs):
         """if user filter is not passed, default to request.user"""
         super().__init__(*args, **kwargs)
-        if self.data.get("user") is None:
+        if self.data.get("user_id") is None:
             # default filtering by current user
             self.data = dict(self.data)
             self.data["user"] = kwargs.get("request").user.id
+
+        # fixme: if there is no filter, handler may return too much data
         # if user == "any", remove the filter
-        if self.data.get("user") == "any":
+        if self.data.get("user_id") == "any":
             self.data = dict(self.data)
-            self.data.pop("user")
+            self.data.pop("user_id")
 
     project = filters.Filter(method=project_id_filter)
+    user_id = filters.Filter(method=user_id_filter)
 
     class Meta:
         model = Invite
-        fields = ("project", "user")
+        fields = ("project", "user_id")
