26e38f7ca4173d9fd28602d2f4e845a65668e484
Toksi86 <puma-yellow@yandex.ru>
2025-06-11 13:16:31 +0500

    Для действия Подтверждение пользователя добавлено автоматическое выставление даты верификации

--- users/admin.py
+++ users/admin.py
@@ -1,33 +1,34 @@
+import urllib.parse
 from datetime import date
 
 import tablib
-import urllib.parse
 from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth.models import Permission
 from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.urls import path
+from django.utils.timezone import now
 
+from core.admin import SkillToObjectInline
 from core.utils import XlsxFileToExport
 from mailing.views import MailingTemplateRender
 from users.services.users_activity import UserActivityDataPreparer
-from .helpers import send_verification_completed_email, force_verify_user
+
+from .helpers import force_verify_user, send_verification_completed_email
 from .models import (
     CustomUser,
-    UserAchievement,
-    Member,
-    Mentor,
     Expert,
     Investor,
-    UserLink,
+    Member,
+    Mentor,
+    UserAchievement,
     UserEducation,
-    UserWorkExperience,
-    UserSkillConfirmation,
     UserLanguages,
+    UserLink,
+    UserSkillConfirmation,
+    UserWorkExperience,
 )
-
-from core.admin import SkillToObjectInline
 
 admin.site.register(Permission)
 
@@ -55,7 +56,7 @@ class UserLanguagesInline(admin.TabularInline):
 
 @admin.action(description="Сделать выбранных пользователей подтверждёнными")
 def make_active(modeladmin, request, queryset):
-    queryset.update(is_active=True)
+    queryset.update(is_active=True, verification_date=now().date())
 
 
 @admin.register(CustomUser)
@@ -268,7 +269,9 @@ class CustomUserAdmin(admin.ModelAdmin):
             binary_data_to_export,
             content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
         )
-        response["Content-Disposition"] = f'attachment; filename*=UTF-8\'\'{encoded_file_name}'
+        response["Content-Disposition"] = (
+            f"attachment; filename*=UTF-8''{encoded_file_name}"
+        )
 
         return response
 
@@ -321,7 +324,9 @@ class CustomUserAdmin(admin.ModelAdmin):
                     baby.first_name + " " + baby.last_name,
                     today.year - baby.birthday.year,
                     ", ".join(interests),
-                    "; ".join(baby.education.values_list("organization_name", flat=True)),
+                    "; ".join(
+                        baby.education.values_list("organization_name", flat=True)
+                    ),
                     baby.v2_speciality if baby.v2_speciality else baby.speciality,
                     baby.email,
                 ]
@@ -337,7 +342,9 @@ class CustomUserAdmin(admin.ModelAdmin):
                     big_man.first_name + " " + big_man.last_name,
                     today.year - big_man.birthday.year,
                     ", ".join(industry_names),
-                    "; ".join(big_man.education.values_list("organization_name", flat=True)),
+                    "; ".join(
+                        big_man.education.values_list("organization_name", flat=True)
+                    ),
                     big_man.v2_speciality
                     if big_man.v2_speciality
                     else big_man.speciality,
