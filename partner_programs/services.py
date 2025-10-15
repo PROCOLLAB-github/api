d2cc7407246df4f15ee497ac73ffbe3704a9cdd6
Toksi <puma-yellow@yandex.ru>
2025-10-15 12:01:47 +0500

    Код приведён в соответствие с PEP8

--- partner_programs/services.py
+++ partner_programs/services.py
@@ -3,7 +3,6 @@ import logging
 from partner_programs.models import PartnerProgramUserProfile
 from project_rates.models import Criteria, ProjectScore
 
-
 logger = logging.getLogger()
 
 
@@ -23,16 +22,14 @@ class ProjectScoreDataPreparer:
         "Класс_курс": "ОШИБКА",
     }
 
-    EXPERT_ERROR_FIELDS = {
-        "Фамилия эксперта": "ОШИБКА"
-    }
+    EXPERT_ERROR_FIELDS = {"Фамилия эксперта": "ОШИБКА"}
 
     def __init__(
         self,
         user_profiles: dict[int, PartnerProgramUserProfile],
         scores: dict[int, list[ProjectScore]],
         project_id: int,
-        program_id: int
+        program_id: int,
     ):
         self._project_id = project_id
         self._user_profiles = user_profiles
@@ -41,35 +38,54 @@ class ProjectScoreDataPreparer:
 
     def get_project_user_info(self) -> dict[str, str]:
         try:
-            user_program_profile: PartnerProgramUserProfile = self._user_profiles.get(self._project_id)
-            user_program_profile_json: dict = user_program_profile.partner_program_data if user_program_profile else {}
+            user_program_profile: PartnerProgramUserProfile = self._user_profiles.get(
+                self._project_id
+            )
+            user_program_profile_json: dict = (
+                user_program_profile.partner_program_data if user_program_profile else {}
+            )
 
             user_info: dict[str, str] = {
-                "Фамилия": user_program_profile.user.last_name if user_program_profile else '',
-                "Имя": user_program_profile.user.first_name if user_program_profile else '',
-                "Отчество": user_program_profile.user.patronymic if user_program_profile else '',
+                "Фамилия": (
+                    user_program_profile.user.last_name if user_program_profile else ""
+                ),
+                "Имя": (
+                    user_program_profile.user.first_name if user_program_profile else ""
+                ),
+                "Отчество": (
+                    user_program_profile.user.patronymic if user_program_profile else ""
+                ),
                 "Email": (
-                    user_program_profile_json.get('email') if user_program_profile_json.get('email')
+                    user_program_profile_json.get("email")
+                    if user_program_profile_json.get("email")
                     else user_program_profile.user.email
                 ),
-                "Регион_РФ": user_program_profile_json.get('region', ''),
-                "Учебное_заведение": user_program_profile_json.get('education_type', ''),
-                "Название_учебного_заведения": user_program_profile_json.get('institution_name', ''),
-                "Класс_курс": user_program_profile_json.get('class_course', ''),
+                "Регион_РФ": user_program_profile_json.get("region", ""),
+                "Учебное_заведение": user_program_profile_json.get("education_type", ""),
+                "Название_учебного_заведения": user_program_profile_json.get(
+                    "institution_name", ""
+                ),
+                "Класс_курс": user_program_profile_json.get("class_course", ""),
             }
             return user_info
         except Exception as e:
-            logger.error(f"Prepare export rates data about user error: {str(e)}", exc_info=True)
+            logger.error(
+                f"Prepare export rates data about user error: {str(e)}", exc_info=True
+            )
             return self.USER_ERROR_FIELDS
 
     def get_project_expert_info(self) -> dict[str, str]:
         try:
             project_scores: list[ProjectScore] = self._scores.get(self._project_id, [])
             first_score = project_scores[0] if project_scores else None
-            expert_last_name: dict[str, str] = {"Фамилия эксперта": first_score.user.last_name if first_score else ''}
+            expert_last_name: dict[str, str] = {
+                "Фамилия эксперта": first_score.user.last_name if first_score else ""
+            }
             return expert_last_name
         except Exception as e:
-            logger.error(f"Prepare export rates data about expert error: {str(e)}", exc_info=True)
+            logger.error(
+                f"Prepare export rates data about expert error: {str(e)}", exc_info=True
+            )
             return self.EXPERT_ERROR_FIELDS
 
     def get_project_scores_info(self) -> dict[str, str]:
@@ -78,16 +94,29 @@ class ProjectScoreDataPreparer:
             project_scores: list[ProjectScore] = self._scores.get(self._project_id, [])
             score_info_with_out_comment: dict[str, str] = {
                 score.criteria.name: score.value
-                for score in project_scores if score.criteria.name != "Комментарий"
+                for score in project_scores
+                if score.criteria.name != "Комментарий"
             }
             project_scores_dict.update(score_info_with_out_comment)
-            comment = next((score for score in project_scores if score.criteria.name == "Комментарий"), None)
+            comment = next(
+                (
+                    score
+                    for score in project_scores
+                    if score.criteria.name == "Комментарий"
+                ),
+                None,
+            )
             if comment is not None:
                 project_scores_dict["Комментарий"] = comment.value
             return project_scores_dict
         except Exception as e:
-            logger.error(f"Prepare export rates data about project_scores error: {str(e)}", exc_info=True)
+            logger.error(
+                f"Prepare export rates data about project_scores error: {str(e)}",
+                exc_info=True,
+            )
             return {
                 criteria.name: "ОШИБКА"
-                for criteria in Criteria.objects.filter(partner_program__id=self._program_id)
+                for criteria in Criteria.objects.filter(
+                    partner_program__id=self._program_id
+                )
             }
