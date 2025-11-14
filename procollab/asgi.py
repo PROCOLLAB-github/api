80774dc8ccfc2fee7a199849c649440078665fef
Toksi <puma-yellow@yandex.ru>
2025-11-14 14:23:56 +0500

    Бэкенд переключен на Daphne + фиксы логирования/импортов

--- procollab/asgi.py
+++ procollab/asgi.py
@@ -1,16 +1,19 @@
 import os
-import chats.routing
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-from chats.middleware import TokenAuthMiddleware
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "procollab.settings")
+
+# Ensure Django app registry is loaded before importing project routes.
+django_asgi_app = get_asgi_application()
+
+import chats.routing  # noqa: E402
+from chats.middleware import TokenAuthMiddleware  # noqa: E402
 
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_app,
         "websocket": TokenAuthMiddleware(URLRouter(chats.routing.websocket_urlpatterns)),
     }
 )
--- scripts/startup.sh
+++ scripts/startup.sh
@@ -2,4 +2,6 @@
 
 python manage.py migrate
 python manage.py collectstatic --no-input
-python manage.py runserver 0.0.0.0:8000
+
+# Use Daphne ASGI server instead of Django's dev server.
+exec daphne -b 0.0.0.0 -p 8000 procollab.asgi:application
