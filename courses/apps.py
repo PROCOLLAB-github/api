1c14f447a8b21fd1a4a7a1097944f5ceee9a3fab
Toksi <Puma-Yellow@yandex.ru>
2026-02-26 11:44:17 +0500

    Новые модели добавлены в панель администратора, протестировано ручное создание объектов

--- courses/apps.py
+++ courses/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class CoursesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'courses'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "courses"
+    verbose_name = "Курсы"
